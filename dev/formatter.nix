2df14df6693c05d62fa384cd7d160f26ca555ca3
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-08 00:24:26 +0800

    initial commits

---
+++ dev/formatter.nix
@@ -0,0 +1,35 @@
+{inputs, ...}: {
+  imports = [inputs.treefmt-nix.flakeModule];
+  perSystem = {
+    treefmt = {
+      # Used to find the project root
+      projectRootFile = ".git/config";
+
+      programs = {
+        # Nix formatters & linters
+        alejandra.enable = true;
+        deadnix.enable = true;
+        statix.enable = true;
+
+        # Python formatters & linters
+        ruff-check.enable = true;
+        ruff-format.enable = true;
+
+        # Shell formatters & linter
+        shellcheck.enable = true;
+        shfmt.enable = true;
+
+        # Other formatters
+        keep-sorted.enable = true;
+        typos.enable = true;
+        yamlfmt.enable = true;
+        taplo.enable = true;
+      };
+
+      settings.global.excludes = [
+        "*.lock"
+        ".gitignore"
+      ];
+    };
+  };
+}
