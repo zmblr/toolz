4547e43e90c5c067c6a549a61062cbbe952ee5b9
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-10 15:57:37 +0900

    feat(pkgs): Add nextflow

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -8,6 +8,7 @@
       {
         bbtools = pkgs.callPackage ./bbtools.nix {};
         fastaptamer = pkgs.callPackage ./fastaptamer.nix {};
+        nextflow = pkgs.callPackage ./nextflow/package.nix {};
       }
       // lib.optionalAttrs pkgs.stdenv.isLinux {
         blast = pkgs.callPackage ./blast.nix {};
