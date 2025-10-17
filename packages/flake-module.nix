e6e5ba7c6f024db4f8f00ce7071686d9bb4af2a3
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-17 15:58:51 +0900

    feat(pkgs): Add cutadapt and its dependencies

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -7,9 +7,12 @@
     packages =
       {
         bbtools = pkgs.callPackage ./bbtools.nix {};
+        cutadapt = pkgs.callPackage ./cutadapt.nix {};
+        dnaio = pkgs.callPackage ./dnaio.nix {};
         fastaptamer = pkgs.callPackage ./fastaptamer.nix {};
-        nextflow = pkgs.callPackage ./nextflow {};
         flash = pkgs.callPackage ./flash.nix {};
+        nextflow = pkgs.callPackage ./nextflow {};
+        xopen = pkgs.callPackage ./xopen.nix {};
       }
       // lib.optionalAttrs pkgs.stdenv.isLinux {
         blast = pkgs.callPackage ./blast.nix {};
