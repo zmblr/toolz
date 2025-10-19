59b3e55f1c0326aecda52ef1ee90032fa7fe987b
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-19 20:40:50 +0900

    feat(pkgs): Add jellyfish

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -6,14 +6,18 @@
   }: {
     packages =
       {
+        # keep-sorted start
+        aptasuite = pkgs.callPackage ./aptasuite.nix {};
         bbtools = pkgs.callPackage ./bbtools.nix {};
         cutadapt = pkgs.callPackage ./cutadapt.nix {};
         dnaio = pkgs.callPackage ./dnaio.nix {};
         fastaptamer = pkgs.callPackage ./fastaptamer.nix {};
         flash = pkgs.callPackage ./flash.nix {};
+        jellyfish = pkgs.callPackage ./jellyfish.nix {};
+        jellyfish-full = pkgs.callPackage ./jellyfish-full.nix {};
         nextflow = pkgs.callPackage ./nextflow {};
         xopen = pkgs.callPackage ./xopen.nix {};
-        aptasuite = pkgs.callPackage ./aptasuite.nix {};
+        # keep-sorted end
       }
       // lib.optionalAttrs pkgs.stdenv.isLinux {
         blast = pkgs.callPackage ./blast.nix {};
