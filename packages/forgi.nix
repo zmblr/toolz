b69104ea90213f8104a793107e96659de40e8c3f
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-23 15:46:46 +0900

    feat(pkgs): Add forgi

---
+++ packages/forgi.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  numpy,
+  scipy,
+  pandas,
+  future,
+  networkx,
+  biopython,
+  appdirs,
+  logging_exceptions,
+  cython,
+  matplotlib,
+  beautifulsoup4,
+  scikit-learn,
+}:
+buildPythonPackage rec {
+  pname = "forgi";
+  version = "2.2.3";
+  format = "setuptools";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-yHpFWxvfkfkcFGJ3ftoyzhND+xSbaIpAbfPXzry3eeo=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+    cython
+    numpy
+  ];
+
+  propagatedBuildInputs = [
+    numpy
+    scipy
+    pandas
+    future
+    networkx
+    biopython
+    appdirs
+    logging_exceptions
+    cython
+    matplotlib
+    beautifulsoup4
+    scikit-learn
+  ];
+
+  pythonImportsCheck = ["forgi"];
+
+  meta = with lib; {
+    description = "RNA Graph Library";
+    homepage = "http://www.tbi.univie.ac.at/~pkerp/forgi/";
+    license = licenses.gpl3Only;
+    maintainers = [];
+  };
+}
