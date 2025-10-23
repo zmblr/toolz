669b59b5682d568db8cee8c2c3cfafe290947fd7
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-23 15:46:46 +0900

    feat(pkgs): Add forgi

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -5,7 +5,7 @@
     ...
   }: {
     packages =
-      {
+      rec {
         # keep-sorted start
         aptasuite = pkgs.callPackage ./aptasuite.nix {};
         bbtools = pkgs.callPackage ./bbtools.nix {};
@@ -13,10 +13,12 @@
         dnaio = pkgs.callPackage ./dnaio.nix {};
         fastaptamer = pkgs.callPackage ./fastaptamer.nix {};
         flash = pkgs.callPackage ./flash.nix {};
+        forgi = pkgs.python3Packages.callPackage ./forgi.nix {inherit logging_exceptions;};
         jellyfish = pkgs.callPackage ./jellyfish.nix {};
         jellyfish-full = pkgs.callPackage ./jellyfish-full.nix {};
         kmc = pkgs.callPackage ./kmc.nix {};
         kmc-full = pkgs.callPackage ./kmc-full.nix {};
+        logging_exceptions = pkgs.python3Packages.callPackage ./logging-exceptions.nix {};
         nextflow = pkgs.callPackage ./nextflow {};
         xopen = pkgs.callPackage ./xopen.nix {};
         # keep-sorted end
