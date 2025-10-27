05a6844b4e646747a39fd53431237e51338490be
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-27 17:33:44 +0900

    feat: Add seqtable

--- packages/lib/make-python-packages.nix
+++ packages/lib/make-python-packages.nix
@@ -2,13 +2,22 @@
   lib,
   callPackage,
   stdenv,
+  inputs ? {},
+  system ? null,
 }: let
   byNamePackage = name: let
     firstTwo = builtins.substring 0 2 name;
   in
     ../by-name + "/${firstTwo}/${name}/package.nix";
+
+  externalPythonPackages = _pySelf: _pySuper:
+    if inputs != {} && system != null
+    then {
+      # some-py-pkg = inputs.some-flake.packages.${system}.some-py-pkg or null;
+    }
+    else {};
 in
-  pySelf: _pySuper:
+  pySelf: pySuper:
     {
       # keep-sorted start
       cutadapt = pySelf.callPackage (byNamePackage "cutadapt") {};
@@ -18,6 +27,7 @@ in
       xopen = pySelf.callPackage (byNamePackage "xopen") {};
       # keep-sorted end
     }
+    // (externalPythonPackages pySelf pySuper)
     // lib.optionalAttrs stdenv.isLinux {
       viennarna-hpc = pySelf.toPythonModule (
         callPackage (byNamePackage "viennarna-hpc") {
