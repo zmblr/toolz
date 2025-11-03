4358716146e5de7696fbb651161fd0d5892b57fb
mulatta <67085791+mulatta@users.noreply.github.com>
2025-11-03 12:46:42 +0900

    feat(templates): Add uv template

---
+++ templates/uv/nix/formatter.nix
@@ -0,0 +1,34 @@
+{inputs, ...}: {
+  imports = [inputs.treefmt-nix.flakeModule];
+  perSystem = {
+    treefmt = {
+      projectRootFile = ".git/config";
+
+      programs = {
+        # Nix formatters & linters
+        alejandra.enable = true;
+        deadnix.enable = true;
+        statix.enable = true;
+
+        # Python formatters & linters
+        ruff-check.enable = true;
+        ruff-format.enable = true;
+
+        # Shell formatters & linter
+        shellcheck.enable = true;
+        shfmt.enable = true;
+
+        # Other formatters
+        keep-sorted.enable = true;
+        yamlfmt.enable = true;
+        taplo.enable = true;
+      };
+
+      settings.global.excludes = [
+        "*.lock"
+        ".gitignore"
+        "results/**"
+      ];
+    };
+  };
+}
