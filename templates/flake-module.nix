991d4e72844da6061219e88e2e80128d913456d9
mulatta <67085791+mulatta@users.noreply.github.com>
2025-11-21 19:30:50 +0900

    feat(templates): Add pixi template

--- templates/flake-module.nix
+++ templates/flake-module.nix
@@ -4,5 +4,9 @@
       path = ./rust;
       description = "Basic toolz rust project template";
     };
+    pixi = {
+      path = ./pixi;
+      description = "Basic toolz pixi project template";
+    };
   };
 }
