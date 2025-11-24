2c4264384d1988c5739f76e0302f561bbfce4a70
mulatta <67085791+mulatta@users.noreply.github.com>
2025-11-24 14:34:53 +0900

    fix(lib): Complete isolation of version-conflicting packages
    BREAKING CHANGE: Remove jax, jaxlib, jaxtyping, typeguard, and pyarrow from global overlay
    
    See jj commit b4c7e4fe for full details

--- packages/by-name/al/alphafold3/base.nix
+++ packages/by-name/al/alphafold3/base.nix
@@ -3,6 +3,7 @@
   buildPythonPackage,
   fetchFromGitHub,
   fetchurl,
+  fetchPypi,
   # Build
   cmake,
   ninja,
@@ -24,18 +25,14 @@
   chex,
   dm-haiku,
   dm-tree,
-  jax,
-  jaxlib,
   jax-cuda12-plugin,
   jax-triton,
-  jaxtyping,
   numpy,
   pillow,
   rdkit,
   scipy,
   triton,
   tqdm,
-  typeguard,
   zstandard,
   # CUDA libraries
   nvidia-cuda-runtime-cu12,
@@ -47,7 +44,128 @@
   nvidia-cuda-cupti-cu12,
   nvidia-nccl-cu12,
   nvidia-nvjitlink-cu12,
+  # Build dependencies for local JAX packages
+  setuptools,
+  wheel,
+  ml-dtypes,
+  opt-einsum,
+  flatbuffers,
+  autoPatchelfHook,
+  stdenv,
+  python,
+  hatchling,
+  setuptools-scm,
+  typing-extensions,
 }: let
+  # Local JAX ecosystem for AlphaFold3 (version 0.4.34)
+  # NOTE: Not exposed globally to avoid conflicts with nixpkgs JAX 0.6.0
+  # Local typeguard 2.13.3 (required by jaxtyping 0.2.34)
+  typeguard-local = buildPythonPackage rec {
+    pname = "typeguard";
+    version = "2.13.3";
+    pyproject = true;
+
+    src = fetchFromGitHub {
+      owner = "agronholm";
+      repo = "typeguard";
+      rev = "refs/tags/${version}";
+      hash = "sha256-+WbfS+qzc2vFpYQ0PFME9UhZn9yL7qjdCxTV8cLivNk=";
+    };
+
+    build-system = [setuptools setuptools-scm];
+    dependencies = [typing-extensions];
+    env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
+    pythonImportsCheck = ["typeguard"];
+    doCheck = false;
+  };
+
+  # Local jaxlib 0.4.34 (prebuilt wheel)
+  jaxlib-local = let
+    inherit (python) pythonVersion;
+
+    srcs = {
+      "3.10-x86_64-linux" = fetchPypi {
+        pname = "jaxlib";
+        version = "0.4.34";
+        format = "wheel";
+        python = "cp310";
+        abi = "cp310";
+        platform = "manylinux2014_x86_64";
+        hash = "sha256-2A5mSIVUbm1RbnJh/j0VnQpHq/V/vugpnzc2chg9wtI=";
+      };
+      "3.11-x86_64-linux" = fetchPypi {
+        pname = "jaxlib";
+        version = "0.4.34";
+        format = "wheel";
+        python = "cp311";
+        abi = "cp311";
+        platform = "manylinux2014_x86_64";
+        hash = "sha256-O8/jpDpkqaMo+KPKoQzXe8jbEz9X8F3VL6w2Oyq1v18=";
+      };
+      "3.12-x86_64-linux" = fetchPypi {
+        pname = "jaxlib";
+        version = "0.4.34";
+        format = "wheel";
+        python = "cp312";
+        abi = "cp312";
+        platform = "manylinux2014_x86_64";
+        hash = "sha256-SCcukDT/ho1DKM8AVaB4gv0r6T9Z37YoOvfeSR+dEpA=";
+      };
+    };
+  in
+    buildPythonPackage {
+      pname = "jaxlib";
+      version = "0.4.34";
+      format = "wheel";
+
+      src = srcs."${pythonVersion}-${stdenv.hostPlatform.system}"
+      or (throw "jaxlib 0.4.34 is not supported on ${stdenv.hostPlatform.system} with Python ${pythonVersion}");
+
+      nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [autoPatchelfHook];
+      buildInputs = [(lib.getLib stdenv.cc.cc)];
+      dependencies = [absl-py flatbuffers ml-dtypes scipy];
+      pythonImportsCheck = ["jaxlib"];
+    };
+
+  # Local jax 0.4.34 (source build)
+  jax-local = buildPythonPackage rec {
+    pname = "jax";
+    version = "0.4.34";
+    pyproject = true;
+
+    src = fetchFromGitHub {
+      owner = "jax-ml";
+      repo = "jax";
+      rev = "refs/tags/jax-v${version}";
+      hash = "sha256-f49YECYVkb5NpG/5GSSVW3D3J0Lruq2gI62iiXSOHkw=";
+    };
+
+    build-system = [setuptools wheel];
+    dependencies = [jaxlib-local ml-dtypes numpy opt-einsum scipy];
+    env.JAX_RELEASE = "1";
+    pythonImportsCheck = ["jax"];
+    doCheck = false;
+  };
+
+  # Local jaxtyping 0.2.34 (with local typeguard)
+  jaxtyping-local = buildPythonPackage rec {
+    pname = "jaxtyping";
+    version = "0.2.34";
+    pyproject = true;
+
+    src = fetchFromGitHub {
+      owner = "patrick-kidger";
+      repo = "jaxtyping";
+      rev = "refs/tags/v${version}";
+      hash = "sha256-/BWa3USKO/WzT2YDOnCdcrv2qmmHEPSfMhgOt9LHQYE=";
+    };
+
+    build-system = [hatchling];
+    dependencies = [typeguard-local typing-extensions];
+    pythonImportsCheck = ["jaxtyping"];
+    doCheck = false;
+  };
+
   # PDB snapshot (2025-01-01) for reproducibility
   componentsCif = fetchurl {
     url = "https://pdbsnapshots.s3-us-west-2.amazonaws.com/20250101/pub/pdb/data/monomers/components.cif.gz";
@@ -151,18 +269,17 @@ in
       chex
       dm-haiku
       dm-tree
-      jax
-      jaxlib
+      jax-local # Local JAX 0.4.34
+      jaxlib-local # Local jaxlib 0.4.34
       jax-cuda12-plugin
       jax-triton
-      jaxtyping
+      jaxtyping-local # Local jaxtyping 0.2.34 with typeguard 2.13.3
       numpy
       pillow
       rdkit
       scipy
       triton
       tqdm
-      typeguard
       zstandard
       nvidia-cuda-runtime-cu12
       nvidia-cublas-cu12
