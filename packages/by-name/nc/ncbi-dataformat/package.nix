51115600e32a7382e585344b40aacd632de41bd5
mulatta <67085791+mulatta@users.noreply.github.com>
2025-11-30 01:03:56 +0900

    chore(ncbi-datacli): migrate upstream source from ftp to github

--- packages/by-name/nc/ncbi-dataformat/package.nix
+++ packages/by-name/nc/ncbi-dataformat/package.nix
@@ -1,31 +1,41 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchzip,
   autoPatchelfHook,
 }:
 stdenv.mkDerivation rec {
-  pname = "ncbi-datasets-cli";
-  version = "undefined";
+  pname = "ncbi-dataformat";
+  version = "18.10.2";
 
   src =
     if stdenv.isLinux && stdenv.isx86_64
     then
-      fetchurl {
-        url = "https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/dataformat";
-        sha256 = "sha256-qdCmLHvyd+96HZrMKI3OfuBFFbHwyq99lO/ik9bDa+M=";
+      fetchzip {
+        url = "https://github.com/ncbi/datasets/releases/download/v${version}/linux-amd64.cli.package.zip";
+        sha256 = "sha256-0F/WyG76bc50MFC3wBXa+yxjsAkY2Sw0BLAM9lAl9n4=";
+        stripRoot = false;
       }
-    else if stdenv.isDarwin
+    else if stdenv.isDarwin && stdenv.isx86_64
     then
-      fetchurl {
-        url = "https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/mac/dataformat";
-        sha256 = lib.fakeSha256;
+      fetchzip {
+        url = "https://github.com/ncbi/datasets/releases/download/v${version}/darwin-amd64.cli.package.zip";
+        sha256 = "sha256-F8qqci56CKik82uXTg3Nqzfy5VByoJICikb8R/K2Q7o=";
+        stripRoot = false;
+      }
+    else if stdenv.isDarwin && stdenv.isAarch64
+    then
+      fetchzip {
+        url = "https://github.com/ncbi/datasets/releases/download/v${version}/darwin-arm64.cli.package.zip";
+        sha256 = "sha256-rBrXK8GVMhtpHQ5o2T3tRKXvOcnzrraBb1gyxlYsQOA=";
+        stripRoot = false;
       }
     else if stdenv.isLinux && stdenv.isAarch64
     then
-      fetchurl {
-        url = "https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-arm64/dataformat";
-        sha256 = lib.fakeSha256;
+      fetchzip {
+        url = "https://github.com/ncbi/datasets/releases/download/v${version}/linux-arm64.cli.package.zip";
+        sha256 = "sha256-xTarEH5O1Fv/eE4SWTj6Pg4/55IJDDMAaEenfeIWVAk=";
+        stripRoot = false;
       }
     else throw "Unsupported platform";
 
@@ -33,7 +43,6 @@ stdenv.mkDerivation rec {
     autoPatchelfHook
   ];
 
-  dontUnpack = true;
   dontBuild = true;
 
   installPhase = ''
@@ -41,13 +50,13 @@ stdenv.mkDerivation rec {
 
     mkdir -p $out/bin
 
-    install -Dm755 ${src} $out/bin/dataformat
+    install -Dm755 $src/dataformat $out/bin/dataformat
 
     runHook postInstall
   '';
 
   meta = with lib; {
-    description = "NCBI Datasets command-line tools for downloading biological sequence data";
+    description = "NCBI Datasets dataformat tool for converting metadata formats";
     longDescription = ''
       The NCBI Datasets command-line tools consist of dataformat.
       Use datasets to download biological sequence data across all domains of life from NCBI.
@@ -57,7 +66,6 @@ stdenv.mkDerivation rec {
     changelog = "https://github.com/ncbi/datasets/releases";
     license = licenses.publicDomain;
     platforms = platforms.unix;
-    maintainers = [];
-    mainProgram = "datasets";
+    mainProgram = "dataformat";
   };
 }
